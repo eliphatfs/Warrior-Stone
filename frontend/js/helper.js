c0d113783fa8ab4ba2880c3ca06f6ef2ab615f7e
?????·è?? <shiruoxi2010@163.com>
2019-05-18 14:28:36 +0800

    Random Int Helper Function

--- frontend/js/helper.js
+++ frontend/js/helper.js
@@ -12,4 +12,22 @@ function doAlert(hint) {
     alert(hint);
 }
 
+function randInt(low, high) {
+    return low + Math.floor(Math.random() * (high - low));
+}
+
+function getQueryParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    var reg_rewrite = new RegExp("(^|/)" + name + "/([^/]*)(/|$)", "i");
+    var r = window.location.search.substr(1).match(reg);
+    var q = window.location.pathname.substr(1).match(reg_rewrite);
+    if (r != null) {
+        return unescape(r[2]);
+    } else if (q != null) {
+        return unescape(q[2]);
+    } else {
+        return null;
+    }
+}
+
 BASE_ADDR = "http://127.0.0.1:59095"
